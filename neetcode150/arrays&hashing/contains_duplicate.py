0aae3ef83cbf6d4c517309c8ae08e19e744c268e
Darren Huang <darren.y.huang@gmail.com>
2023-05-08 13:56:59 -0400

    added neetcode150

---
+++ neetcode150/arrays&hashing/contains_duplicate.py
@@ -0,0 +1,15 @@
+"""1:55pm
+1:56pm 
+done"""
+
+from typing import List
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for n in nums:
+            if n in seen:
+                return True
+            seen.add(n)
+        return False
