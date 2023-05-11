5a2b9820ba18545b46b39c47ef88c2a8dbe11773
Darren Huang <darren.y.huang@gmail.com>
2023-05-11 15:28:05 -0400

    added graph question

---
+++ neetcode150/11.Graphs/1-num_islands.py
@@ -0,0 +1,32 @@
+"""3:15pm"""
+
+from typing import List
+
+
+def helper_get_children(i, j, grid):
+    n, m = len(grid), len(grid[0])
+    children = [(i - 1, j), (i + 1, j), (i, j + 1), (i, j - 1)]
+    return [c for c in children if c[0] >= 0 and c[0] < n and c[1] >= 0 and c[1] < m]
+
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        seen = set()
+        num_islands = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == "0" or (i, j) in seen:
+                    continue
+                else:
+                    # run dfs on land to get all connected land
+                    stack = [(i, j)]
+                    while stack:
+                        ni, nj = stack.pop()
+                        for c in helper_get_children(ni, nj, grid):
+                            if c not in seen:
+                                seen.add(c)
+                                if grid[c[0]][c[1]] == "1":
+                                    stack.append(c)
+
+                    num_islands += 1
+        return num_islands
